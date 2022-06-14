da9d1f3199e8ad93eca3b00caec931489b0fe671
Alexander <mishinall1991@gmail.com>
2022-06-14 15:54:58 +0300

    first commit

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,20 @@
+import "./style_spinner.css";
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-qtvqibjdt3">
+            <div className="ldio-h0knq6t2aal">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
